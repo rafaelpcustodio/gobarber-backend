714bfef7110e164eb6bf3ee332aa8705386dc470
rafael.custodio <rafael.custodio@dextra-sw.com>
2019-10-20 15:34:45 -0300

    starting JWT config

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rafael Custodio',
-    email: 'rafael@test.com',
-    password_hash: '123456',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
